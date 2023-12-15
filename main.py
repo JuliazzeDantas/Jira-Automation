d4ce399041272f73e27917111a9ba03cd99cb824
Juliasse <vinicius.dantas@raizen.com>
2023-12-15 10:24:41 -0300

    Melhora da segurança na transcrição do arquivo

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import yaml
+
+def main(type, area, user):
+    path = f'folder-test/right_folder/this_folder/{area}.yaml'
+
+    if type == 'insertion':
+        with open(path, 'r') as archive:
+            dict_data = yaml.safe_load(archive)
+            
+        dict_data['spec']['values']['users'].append({'ferramenta' : user})
+        yaml_archive = yaml.dump(dict_data, sort_keys=False)
+
+        with open(path, 'w') as archive:
+            archive.write(yaml_archive)
+        archive.close()
+
+
+    elif type == 'remove':
+        with open(path, 'r') as archive:
+            dict_data = yaml.safe_load(archive)
+
+        dict_data['spec']['values']['users'].remove({'ferramenta' : user})
+        yaml_archive = yaml.dump(dict_data, sort_keys=False)
+
+        with open(path, 'w') as archive:
+            archive.write(yaml_archive)
+        archive.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
